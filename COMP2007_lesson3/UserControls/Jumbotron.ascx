8e1f58b23fb6a46921f4e4f44055c5fbdcb9e90b
blanche0416 <blanche0416@gmail.com>
2016-05-27 14:46:35 +0800

    first commit

---
+++ COMP2007_lesson3/UserControls/Jumbotron.ascx
@@ -0,0 +1,11 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Jumbotron.ascx.cs" Inherits="COMP2007_lesson3.UserControls.Jumbotron" %>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+            <div class="jumbotron">
+                <h1 id="JumbotroH1" runat="server"></h1>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ COMP2007_lesson3/UserControls/Jumbotron.ascx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP2007_lesson3.UserControls
+{
+    
+    public partial class Jumbotron : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            JumbotroH1.InnerText = "Welcome";
+        }
+    }
+}
